1bb20935ed41935bbf2602eacc9b59eaf38a56f3
fabian ignacio salazar sepulveda <energyfabian12@gmail.com>
2022-07-13 21:56:35 -0400

    Form agregar Servicios

--- administrador/forms.py
+++ administrador/forms.py
@@ -1,8 +1,15 @@
+from tkinter import Widget
 from django import forms
 from .models import Servicio
 
 
 class ServicioForm(forms.ModelForm):
+    nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    precio = forms.CharField(widget=forms.NumberInput(attrs={'class': 'form-control'}))
     class Meta:
         model = Servicio
         fields = '__all__'
+
+    
+
+
--- administrador/views.py
+++ administrador/views.py
@@ -1,6 +1,8 @@
+from email import message
 from django.shortcuts import render
 from .models import Servicio
 from .forms import ServicioForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -12,5 +14,14 @@ def servicios(request):
     return render(request, 'servicios/lista.html',{'servicios':servicios})    
 
 def agregarServicio(request):
-
-    return render(request, 'servicios/agregar_servicio.html')
+    data = {
+        'form' : ServicioForm()
+    }    
+    if request.method == 'POST' :
+        formulario  = ServicioForm(data = request.POST, files = request.FILES)
+        if formulario.is_valid():
+            formulario.save()
+            messages.success(request,"Servicio registrado con exito")
+        else:
+            data['form'] = formulario  
+    return render(request, 'servicios/agregar_servicio.html', data)
